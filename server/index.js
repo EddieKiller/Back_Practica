75ed41122fd0802bf4d3c1b9ebc3b1b49477d44d
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2024-07-08 15:46:38 -0400

    Borrar cosas que no uso y crear ListarPropiedad de controllers y ListarPropiedades de routes

--- server/index.js
+++ server/index.js
@@ -11,31 +11,7 @@ app.use(express.urlencoded({ extended: true })); // Para analizar solicitudes co
 app.use(cors());
 app.use(express.json());
 
-const login = require('./routes/Login');
-app.use('/', login);
-
-const comentario = require('./routes/comentario.js');
-app.use('/', comentario);
-
-const usuario = require('./routes/user');
-app.use('/', usuario);
-
-const Registro = require('./routes/Registro');
-app.use('/', Registro);
-
-const biblioteca = require('./routes/Biblioteca');
-app.use('/', biblioteca)
-
-const consulta1 = require('./routes/consultas');
-app.use('/', consulta1);
-
-const Perfil = require('./routes/Perfil')
-app.use('/',Perfil)
-
-const Juego = require('./routes/Juego')
-app.use('/',Juego)
-
-const Lista_juegos = require('./routes/Lista_juegos')
-app.use('/',Lista_juegos)
+const ListarPropiedades = require('./routes/ListarPropiedades')
+app.use('/', ListarPropiedades)
 
 app.listen(4001, () => console.log('Inicio de servidor'));
