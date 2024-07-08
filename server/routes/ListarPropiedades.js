1b4386f78e7e7c227083f0b058f79b4513210a33
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2024-07-08 15:50:24 -0400

    arreglado error de tipeo, ahora devuelve lista de propiedades

--- server/routes/ListarPropiedades.js
+++ server/routes/ListarPropiedades.js
@@ -1,7 +1,7 @@
 // router.js
 const express = require('express');
 const router = express.Router();
-const listarPropiedades = require('../controllers/ListarPropiedades');
+const listarPropiedades = require('../controllers/ListarPropiedad');
 
 // Configura una ruta para mostrar juegos
 router.get('/ListarPropiedad', listarPropiedades);
